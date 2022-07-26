61a243754ca4feecc7f2f53f9c015ab9a28daf55
silvanors <silvanors@gmail.com>
2022-07-26 02:21:12 -0300

    TypeScript com o outDir e rootDir

---
+++ src/exemplos/any-unknown-never.ts
@@ -0,0 +1,30 @@
+//uso do "any" com "unknown" é uma boa prática, como mostra o exemplo abaixo.
+
+let anyEstaDeVolta: any;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+
+let stringTest: string ='verificar';
+stringTest = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'vai sim';
+
+let stringTest2: string = 'agora vai';
+stringTest2 = unknownValor;
+
+if(typeof unknownValor === 'string') {
+    stringTest2 = unknownValor;
+}
+
+// uso do "throw" que interrompe o código pode ser usado o "void" ou o "never" na hora de tipar a função.
+
+function jogaErro(erro: string, codigo: number): never {
+    throw {error: erro, code: codigo}
+}
+
+jogaErro('deu erro', 500);
